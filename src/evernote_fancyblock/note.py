e4a726838baba76be29ab3e0c7f4f172cda90d7a
ttimasdf <ttimasdf@users.noreply.github.com>
2017-04-30 12:03:05 +0800

    Add utility modules

---
+++ src/evernote_fancyblock/note.py
@@ -0,0 +1,4 @@
+from bs4 import BeautifulSoup
+
+def codeblock_detect(soup):
+    pass
---
+++ src/evernote_fancyblock/utils.py
@@ -0,0 +1,35 @@
+import base64
+
+ENC_KEY=120
+
+def get_token():
+    'TODO: Get an OAuth token from web service'
+    return input("Enter your developer token: ")
+
+
+def set_from_range(input_numbers):
+    selected_numbers = []
+    for part in input_numbers.split(','):
+            x = part.split('-')
+            if len(x) == 1:
+                selected_numbers.append(int(x[0]))
+            elif len(x) == 2:
+                selected_numbers.extend(range(int(x[0]), int(x[1])+1))
+            else:
+                print("Error, your input seems illegal." + str(len(x)))
+                return None
+    return set(selected_numbers)
+
+
+def encrypt(text, key=ENC_KEY):
+    if isinstance(text, str):
+        text = text.encode()
+    assert isinstance(text, bytes)
+    return base64.b64encode(bytes(c^key for c in text))
+
+
+def decrypt(text, key=ENC_KEY):
+    if isinstance(text, str):
+        text = text.encode()
+    assert isinstance(text, bytes)
+    return bytes(c^key for c in base64.b64decode(text))
